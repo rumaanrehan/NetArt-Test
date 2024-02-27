1b8a8c9493ccd4f813064f5ecf7ad63832aa49cf
rumaanrehan <98449501+rumaanrehan@users.noreply.github.com>
2024-02-27 12:38:41 +0530

    Add files via upload

---
+++ src/components/Foot.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Foot = () => {
+  return (
+    <div className='container foot'>
+        <div><i class="fa-solid fa-phone"></i>Toll free 1800 200 1234</div>
+        <div><i class="fa-brands fa-facebook"></i>www.facebook.com/cripumps</div>
+        <div><i class="fa-solid fa-globe"></i>www.crigroups.com</div>
+    </div>
+  )
+}
+
+export default Foot
